1ffe909c7844a9600454c1a8ece13d9f8d09274d
FloweringTre <lilianna.carlton@gmail.com>
2024-03-02 10:14:43 -0700

    Updated item model provider to have new pasture blankets (40-49) and the sheepskin.

--- src/main/java/net/sc4rydreams/blossomeq/datagen/BlossomItemModelProvider.java
+++ src/main/java/net/sc4rydreams/blossomeq/datagen/BlossomItemModelProvider.java
@@ -18,25 +18,28 @@ public class BlossomItemModelProvider extends ItemModelProvider {
     protected void registerModels() {
         // ONE OFFS
         for (int var1 = 0; var1 < 1; ++var1) {
-            ((ItemModelBuilder) this.withExistingParent("pasture_blanket_" + "quilted_black", "item/generated"))
-                .texture("layer0", new ResourceLocation("blossomeq", "items/pasture_blanket/pasture_blanket_" + "quilted_black" + "_icon"));
+//            ((ItemModelBuilder) this.withExistingParent("pasture_blanket_" + "quilted_black", "item/generated"))
+//                .texture("layer0", new ResourceLocation("blossomeq", "items/pasture_blanket/pasture_blanket_" + "quilted_black" + "_icon"));
+//
+//            ((ItemModelBuilder) this.withExistingParent("pasture_blanket_" + "quilted_white", "item/generated"))
+//                    .texture("layer0", new ResourceLocation("blossomeq", "items/pasture_blanket/pasture_blanket_" + "quilted_white" + "_icon"));
 
-            ((ItemModelBuilder) this.withExistingParent("pasture_blanket_" + "quilted_white", "item/generated"))
-                    .texture("layer0", new ResourceLocation("blossomeq", "items/pasture_blanket/pasture_blanket_" + "quilted_white" + "_icon"));
+            ((ItemModelBuilder) this.withExistingParent("sheepskin", "item/generated"))
+                    .texture("layer0", new ResourceLocation("blossomeq", "items/sheepskins/sheepskin"));
         }
 
     // LOOPING - values are one more than you want
-        for (int var1 = 1; var1 < 39; ++var1) {
-            int counter = var1 + 1;
+        for (int var1 = 40; var1 < 50; ++var1) {
+            //int counter = var1 + 1;
             ((ItemModelBuilder) this.withExistingParent("pasture_blanket_neon_" + var1, "item/generated"))
                     .texture("layer0", new ResourceLocation("blossomeq", "item/pasture_blanket/pasture_blanket_neon_" + var1 + "_icon"));
         }
 
-        for (int var1 = 1; var1 < 26; ++var1) {
-            int counter = var1 + 1;
-            ((ItemModelBuilder) this.withExistingParent("transport_boots_" + var1, "item/generated"))
-                    .texture("layer0", new ResourceLocation("blossomeq", "item/transport_boots/transport_boots_" + var1));
-        }
+//        for (int var1 = 1; var1 < 26; ++var1) {
+//            //int counter = var1 + 1;
+//            ((ItemModelBuilder) this.withExistingParent("transport_boots_" + var1, "item/generated"))
+//                    .texture("layer0", new ResourceLocation("blossomeq", "item/transport_boots/transport_boots_" + var1));
+//        }
 
     }
 
